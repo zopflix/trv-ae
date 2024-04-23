d403f82ecc016cb0f1abf0f9c417a4b30f077994
ashumanchanda <ashu.manchanda@zopflix.com>
2024-04-23 16:55:20 +0530

    Terms & Conditions page content update

--- src/app/terms-conditions/page.js
+++ src/app/terms-conditions/page.js
@@ -3,17 +3,17 @@ import TermsCondition from './TermsCondition'
 export default function conditionsTravanyaPage() {
 
     return (
-      <TermsCondition/>
+        <TermsCondition />
     )
 }
 
 export async function generateMetadata() {
     return {
-        title: 'Terms & Conditions - Travanya',
-        description: 'Learn Travanya terms and conditions for a comprehensive understanding of our policies. From booking to travel essentials, find all the information you need here.',
+        title: 'Terms & Conditions - Travanya.ae',
+        description: 'Familiarise yourself with Travanyas terms and conditions to fully comprehend our policies. From booking procedures to travel details, youll find everything here.',
         keywords: ['terms conditions Travanya'],
         openGraph: {
-            title: 'Terms & Conditions - Travanya',
+            title: 'Terms & Conditions - Travanya.ae',
             description: "Learn Travanya terms and conditions for a comprehensive understanding of our policies. From booking to travel essentials, find all the information you need here.",
             url: 'https://www.travanya.com/terms-conditions',
         },
