88b4840a277e83e5864d6691922e5bff4714146c
ashumanchanda <ashu.manchanda@zopflix.com>
2024-04-23 16:58:40 +0530

    Disclaimers page content update

--- src/app/disclaimer/page.js
+++ src/app/disclaimer/page.js
@@ -2,18 +2,18 @@ import Disclaimer from './Disclaimer'
 export default function disclaimerTravanyaPage() {
 
     return (
-       <Disclaimer/>
+        <Disclaimer />
     )
 }
 
 export async function generateMetadata() {
     return {
-        title: 'Disclaimer - Travanya',
-        description: 'By visiting our site, you agree to our disclaimer terms. Should we update, amend or make changes, they’ll be updated here. Your continued use implies consent to all the updates.',
+        title: 'Disclaimer - Travanya.ae',
+        description: 'Familiarise yourself with Travanyas terms and conditions to fully comprehend our policies. From booking procedures to travel details, youll find everything here.',
         keywords: ['Contact Travanya'],
         openGraph: {
-            title: 'Disclaimer - Travanya',
-            description: "By visiting our site, you agree to our disclaimer terms. Should we update, amend or make changes, they’ll be updated here. Your continued use implies consent to all the updates.",
+            title: 'Disclaimer - Travanya.ae',
+            description: "Familiarise yourself with Travanya's terms and conditions to fully comprehend our policies. From booking procedures to travel details, you'll find everything here.",
             url: 'https://www.travanya.com/disclaimer',
         },
     }
