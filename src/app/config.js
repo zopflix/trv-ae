5ff4926837886d2e0677a43faa664748d1afc659
mohit <mohit.grover@zopflix.com>
2024-05-16 10:25:22 +0530

    config url change

--- src/app/config.js
+++ src/app/config.js
@@ -1,8 +1,8 @@
 
-export const apiURL = 'https://api-stg.shipratravel.com/flightSearch/api/';
-export const bookingAPIURL = 'https://api-stg.shipratravel.com/flightbooking/api/';
-export const crmAPIURL = 'https://api-crm-stg.superfares.com/api/';
-export const cmsAPIURL = 'https://api-cms-stg.superfares.com/api/';
+export const apiURL = 'https://api.shipratravel.com/flightSearch/api/';
+export const bookingAPIURL = 'https://api.shipratravel.com/flightbooking/api/';
+export const crmAPIURL = 'https://api-crm.superfares.com/api/';
+export const cmsAPIURL = 'https://api-cms.superfares.com/api/';
 export const tenantId = 7;
 
 export const contactNumber = "+971 506 58 1980";
