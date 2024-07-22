bd1f6bda3629de493272e7db3f1a50079aabbbe6
ashumanchanda <ashu.manchanda@zopflix.com>
2024-07-22 15:19:30 +0530

    peragraph move to bottom in holiday page

--- src/app/components/Domestic-Tour-Packages.js
+++ src/app/components/Domestic-Tour-Packages.js
@@ -32,75 +32,73 @@ export default function DomesticTourPackages(props) {
 
     return (
         <section>
-            <div className="container">
-                {props.isHolidaysPage
-                    ? <div className="row">
-                        {domesticPackages?.map((pkg, ix) => {
-                            return <div className="col-12 col-md-6 col-lg-4 col-xl-3 my-2" key={ix}>
-                                <div className="packageBox position-relative cursor-pointer" onClick={() => window.open(`/holidays/${pkg.slug}-tour-packages`, '_blank')}>
-                                    <div className="packageImgBox">
-                                        <Image
-                                            className="h-auto w-100"
-                                            loader={!!pkg.image ? sfLoader : trvLoader}
-                                            src={!!pkg.image ? pkg.image : "Abu-Dhabi-holiday.webp"}
-                                            alt="domestic-img"
-                                            width={176}
-                                            height={43}
-                                        />
-                                    </div>
-                                    <div className="packageNum position-absolute color-white">{pkg.totalPackages} Packages</div>
-                                    <div className="packageTitleBox bg-white px-3 py-2 rounded-2">
-                                        <div className="d-flex align-items-center justify-content-between">
-                                            <div>
-                                                <p className="mb-0 fs-16 fw-bold color-black">{pkg.name}</p>
-                                            </div>
-                                            <div className="text-end">
-                                                <p className="mb-0 fs-12 fw-bold color-black">From</p>
-                                                <p className="mb-0 fs-16 fw-bold color-orange">{aedNumberFormat(pkg.startingPrice).split('.')[0]}</p>
-                                            </div>
+            {props.isHolidaysPage
+                ? <div className="row">
+                    {domesticPackages?.map((pkg, ix) => {
+                        return <div className="col-12 col-md-6 col-lg-4 col-xl-3 my-2" key={ix}>
+                            <div className="packageBox position-relative cursor-pointer" onClick={() => window.open(`/holidays/${pkg.slug}-tour-packages`, '_blank')}>
+                                <div className="packageImgBox">
+                                    <Image
+                                        className="h-auto w-100"
+                                        loader={!!pkg.image ? sfLoader : trvLoader}
+                                        src={!!pkg.image ? pkg.image : "Abu-Dhabi-holiday.webp"}
+                                        alt="domestic-img"
+                                        width={176}
+                                        height={43}
+                                    />
+                                </div>
+                                <div className="packageNum position-absolute color-white">{pkg.totalPackages} Packages</div>
+                                <div className="packageTitleBox bg-white px-3 py-2 rounded-2">
+                                    <div className="d-flex align-items-center justify-content-between">
+                                        <div>
+                                            <p className="mb-0 fs-16 fw-bold color-black">{pkg.name}</p>
+                                        </div>
+                                        <div className="text-end">
+                                            <p className="mb-0 fs-12 fw-bold color-black">From</p>
+                                            <p className="mb-0 fs-16 fw-bold color-orange">{aedNumberFormat(pkg.startingPrice).split('.')[0]}</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        })}
-                    </div>
-                    : <div className="row">
-                        <div className="col-12 position-relative p-0">
-                            <Slider {...sliderSettings}>
-                                {domesticPackages?.map((pkg, ix) => {
-                                    return <div className="slider-items" key={ix}>
-                                        <div className="packageBox position-relative cursor-pointer" onClick={() => window.open(`/holidays/${pkg.slug}-tour-packages`, '_blank')}>
-                                            <div className="packageImgBox">
-                                                <Image
-                                                    className="h-auto w-100"
-                                                    loader={!!pkg.image ? sfLoader : trvLoader}
-                                                    src={!!pkg.image ? pkg.image : "Abu-Dhabi-holiday.webp"}
-                                                    alt="domestic-img"
-                                                    width={176}
-                                                    height={43}
-                                                />
-                                            </div>
-                                            <div className="packageNum position-absolute color-white">{pkg.totalPackages} Packages</div>
-                                            <div className="packageTitleBox bg-white px-3 py-2 rounded-2">
-                                                <div className="d-flex align-items-center justify-content-between">
-                                                    <div>
-                                                        <p className="mb-0 fs-16 fw-bold color-black">{pkg.name}</p>
-                                                    </div>
-                                                    <div className="text-end">
-                                                        <p className="mb-0 fs-12 fw-bold color-black">From</p>
-                                                        <p className="mb-0 fs-16 fw-bold color-orange">{aedNumberFormat(pkg.startingPrice).split('.')[0]}</p>
-                                                    </div>
+                        </div>
+                    })}
+                </div>
+                : <div className="row">
+                    <div className="col-12 position-relative p-0">
+                        <Slider {...sliderSettings}>
+                            {domesticPackages?.map((pkg, ix) => {
+                                return <div className="slider-items" key={ix}>
+                                    <div className="packageBox position-relative cursor-pointer" onClick={() => window.open(`/holidays/${pkg.slug}-tour-packages`, '_blank')}>
+                                        <div className="packageImgBox">
+                                            <Image
+                                                className="h-auto w-100"
+                                                loader={!!pkg.image ? sfLoader : trvLoader}
+                                                src={!!pkg.image ? pkg.image : "Abu-Dhabi-holiday.webp"}
+                                                alt="domestic-img"
+                                                width={176}
+                                                height={43}
+                                            />
+                                        </div>
+                                        <div className="packageNum position-absolute color-white">{pkg.totalPackages} Packages</div>
+                                        <div className="packageTitleBox bg-white px-3 py-2 rounded-2">
+                                            <div className="d-flex align-items-center justify-content-between">
+                                                <div>
+                                                    <p className="mb-0 fs-16 fw-bold color-black">{pkg.name}</p>
+                                                </div>
+                                                <div className="text-end">
+                                                    <p className="mb-0 fs-12 fw-bold color-black">From</p>
+                                                    <p className="mb-0 fs-16 fw-bold color-orange">{aedNumberFormat(pkg.startingPrice).split('.')[0]}</p>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                })
-                                }
-                            </Slider>
-                        </div>
+                                </div>
+                            })
+                            }
+                        </Slider>
                     </div>
-                }
-            </div>
+                </div>
+            }
         </section>
     )
 }
