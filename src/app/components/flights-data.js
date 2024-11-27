0950e90191d6c39ed5e6853bb7a732f95d8f2262
ashumanchanda <ashu.manchanda@zopflix.com>
2024-11-27 11:46:35 +0530

    Update flights-data.js

--- src/app/components/flights-data.js
+++ src/app/components/flights-data.js
@@ -115,9 +115,9 @@ export default function FlightsData(props) {
                           <h2 className="mn-0 airlineName color-blue fs-20 fw-bold">Georgia</h2>
                           <p className="fs-12 my-2">If you're looking to explore the charming landscapes and rich culture of Georgia, you're in luck! These routes often feature competitive prices, making it easier for travelers to enjoy everything this beautiful country has to offer. </p>
                           <ul className="fs-12 ms-0 ps-3">
-                            <li><a className="color-black" href="#">Dubai to Georgia - </a></li>
-                            <li><a className="color-black" href="#">Abu Dhabi to Georgia - </a></li>
-                            <li><a className="color-black" href="#">Sharjah to Georgia - </a></li>
+                            <li><a className="color-blue" href="#">Dubai to Georgia - </a></li>
+                            <li><a className="color-blue" href="#">Abu Dhabi to Georgia - </a></li>
+                            <li><a className="color-blue" href="#">Sharjah to Georgia - </a></li>
                           </ul>
                         </div>
                       </div>
@@ -147,9 +147,9 @@ export default function FlightsData(props) {
                           <h2 className="mn-0 airlineName color-blue fs-20 fw-bold">Maldives</h2>
                           <p className="fs-12 my-2">The Maldives is a dream destination known for its beautiful beaches & crystal-clear waters. Travelers from the UAE can find great flight options, these routes often offer affordable fares, making it easier to escape to paradise & enjoy a relaxing getaway in this tropical haven.</p>
                           <ul className="fs-12 ms-0 ps-3">
-                            <li><a className="color-black" href="#">Dubai to Maldives - </a></li>
-                            <li><a className="color-black" href="#">Abu Dhabi to Maldives - </a></li>
-                            <li><a className="color-black" href="#">Sharjah to Maldives - </a></li>
+                            <li><a className="color-blue" href="#">Dubai to Maldives - </a></li>
+                            <li><a className="color-blue" href="#">Abu Dhabi to Maldives - </a></li>
+                            <li><a className="color-blue" href="#">Sharjah to Maldives - </a></li>
                           </ul>
                         </div>
                       </div>
@@ -179,9 +179,9 @@ export default function FlightsData(props) {
                           <h2 className="mn-0 airlineName color-blue fs-20 fw-bold">Singapore</h2>
                           <p className="fs-12 my-2">Singapore is a sparkling city-state known for its modern attractions & cultural heritage. Travelers from the UAE can take advantage of affordable flights on popular routes. These routes frequently feature competitive prices, allowing you to explore the iconic sights, delicious cuisine, & diverse neighborhoods of this amazing destination on a budget. </p>
                           <ul className="fs-12 ms-0 ps-3">
-                            <li><a className="color-black" href="#">Dubai to Singapore - </a></li>
-                            <li><a className="color-black" href="#">Abu Dhabi to Singapore - </a></li>
-                            <li><a className="color-black" href="#">Sharjah to Singapore - </a></li>
+                            <li><a className="color-blue" href="#">Dubai to Singapore - </a></li>
+                            <li><a className="color-blue" href="#">Abu Dhabi to Singapore - </a></li>
+                            <li><a className="color-blue" href="#">Sharjah to Singapore - </a></li>
                           </ul>
                         </div>
                       </div>
@@ -211,9 +211,9 @@ export default function FlightsData(props) {
                           <h2 className="mn-0 airlineName color-blue fs-20 fw-bold">Thailand</h2>
                           <p className="fs-12 my-2">Thailand is a popular travel destination famed for its beautiful beaches, crowded cities, & delicious food. From the UAE, travelers can find great flight deals on popular routes. These routes offer reasonable fares, making it easy to experience Thailand's culture, landscapes, & markets, Thailand is just a flight away!</p>
                           <ul className="fs-12 ms-0 ps-3">
-                            <li><a className="color-black" href="#">Dubai to Thailand - </a></li>
-                            <li><a className="color-black" href="#">Abu Dhabi to Thailand - </a></li>
-                            <li><a className="color-black" href="#">Sharjah to Thailand - </a></li>
+                            <li><a className="color-blue" href="#">Dubai to Thailand - </a></li>
+                            <li><a className="color-blue" href="#">Abu Dhabi to Thailand - </a></li>
+                            <li><a className="color-blue" href="#">Sharjah to Thailand - </a></li>
                           </ul>
                         </div>
                       </div>
@@ -243,9 +243,9 @@ export default function FlightsData(props) {
                           <h2 className="mn-0 airlineName color-blue fs-20 fw-bold">USA</h2>
                           <p className="fs-12 my-2">The USA offers a diverse range of experiences, from iconic landmarks to vibrant cities. Travelers from the UAE can find reasonable flight options, that frequently feature competitive prices, making it easier to explore everything. Whether you're planning a family vacation or a solo adventure.</p>
                           <ul className="fs-12 ms-0 ps-3">
-                            <li><a className="color-black" href="#">Dubai to USA - </a></li>
-                            <li><a className="color-black" href="#">Abu Dhabi to USA - </a></li>
-                            <li><a className="color-black" href="#">Sharjah to USA - </a></li>
+                            <li><a className="color-blue" href="#">Dubai to USA - </a></li>
+                            <li><a className="color-blue" href="#">Abu Dhabi to USA - </a></li>
+                            <li><a className="color-blue" href="#">Sharjah to USA - </a></li>
                           </ul>
                         </div>
                       </div>
@@ -275,9 +275,9 @@ export default function FlightsData(props) {
                           <h2 className="mn-0 airlineName color-blue fs-20 fw-bold">Canada</h2>
                           <p className="fs-12 my-2">Canada is a pretty country known for its unique nature & cities. If you're traveling from the UAE, you can find economical flight options these routes often provide attractive fares, allowing you to immerse yourself in Canada's rich culture, landscapes, & friendly communities.  </p>
                           <ul className="fs-12 ms-0 ps-3">
-                            <li><a className="color-black" href="#">Dubai to Canada - </a></li>
-                            <li><a className="color-black" href="#">Abu Dhabi to Canada - </a></li>
-                            <li><a className="color-black" href="#">Sharjah to Canada - </a></li>
+                            <li><a className="color-blue" href="#">Dubai to Canada - </a></li>
+                            <li><a className="color-blue" href="#">Abu Dhabi to Canada - </a></li>
+                            <li><a className="color-blue" href="#">Sharjah to Canada - </a></li>
                           </ul>
                         </div>
                       </div>
