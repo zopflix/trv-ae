58d77141f398236fe9fd35f91e775a35396ea1fc
grovermohit <mohit.grover@zopflix.com>
2025-05-23 09:34:29 +0530

    layout.js page  dataLayer enabled

--- src/app/layout.js
+++ src/app/layout.js
@@ -76,18 +76,18 @@ export default function RootLayout({ children }) {
         }}
         /> */}
 
-        {/* <script dangerouslySetInnerHTML={{
+        <script dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MPQQ8TD6');`,
-        }} /> */}
+              })(window,document,'script','dataLayer','GTM-MKHCK966');`,
+        }} />
       </head>
 
       <body className={inter.className}>
-        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPQQ8TD6"
-          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKHCK966"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         <CounterContextProvider>{children}</CounterContextProvider>
       </body>
     </html>
